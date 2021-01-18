60364a70a32c6f7baad5feb1a6e931c3e37a4d36
nicolai Mortensen <n.bruunmortensen@gmail.com>
2021-01-18 17:18:38 +0100

    page opsætning

---
+++ craftsmen/lib/pages/menu_page.dart
@@ -0,0 +1,8 @@
+import 'package:craftsmen/pages/chat.dart';
+import 'package:flutter/material.dart';
+import 'package:craftsmen/css/style.dart';
+
+class menu_page {
+  @override
+  Widget build(BuildContext, context) {}
+}
