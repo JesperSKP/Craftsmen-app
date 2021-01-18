8e613b2b6d93c5cf77f6106ef66d9fc4c0d61d0e
nicolai Mortensen <n.bruunmortensen@gmail.com>
2021-01-18 17:18:38 +0100

    page opsætning

---
+++ craftsmen/lib/pages/login_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:craftsmen/pages/menu_page.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new _State();
+}
+
+class _State extends State<LoginPage> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+}
---
+++ craftsmen/lib/pages/menu_page.dart
@@ -0,0 +1,5 @@
+class menu_page extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+  
+  }
